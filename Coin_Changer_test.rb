2006cfbd501d05e7d4e7f1fcb8dfd691deb71b4e
casper22875 <casper22875@gmail.com>
2015-09-17 22:21:24 -0400

    added tests for 42cents,two pence,and 99cents

--- Coin_Changer_test.rb
+++ Coin_Changer_test.rb
@@ -30,12 +30,20 @@ def test_for_six_cents
 	coin_changer(6))
 end
 
+def test_for_42_cents
+	assert_equal({penny: 2,nickel: 1,dime: 1,quarter: 1},
+	coin_changer(42))
+end
 
+def test_for_two_pence
+	assert_equal({penny: 3,nickel: 0,dime: 0,quarter: 0},
+	coin_changer(3)) #for the English guy :) based on current exchnage rate(s)
+end
 
-
-
-
-
+def test_for_99_cents
+	assert_equal({penny: 4,nickel: 0,dime: 2,quarter: 3},
+	coin_changer(99))
+end
 
 
 
